1426fdd14d857c1e618aeac63f4d8eb2a2ac8fd4
sureshgatti <gattisuresh2000@gmail.com>
2025-08-21 16:42:54 +0530

    Update outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,3 +1,11 @@
-output "ecr_repo_arn" {
-  value = aws_ecr_repository.catalog.arn
+output "ec2_public_ip" {
+  value = aws_instance.app_server.public_ip
+}
+
+output "ecr_repository_url" {
+  value = aws_ecr_repository.catalog.repository_url
+}
+
+output "vpc_id" {
+  value = aws_vpc.main.id
 }
