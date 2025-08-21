c8a3bd361d4eaf8aff60827d6b35f510d2314868
sureshgatti <gattisuresh2000@gmail.com>
2025-08-21 16:42:22 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,11 +1,19 @@
 variable "region" {
-  description = "AWS region"
-  type        = string
-  default     = "ap-south-1"
+  default = "eu-north-1"
 }
 
-variable "ecr_repo_name" {
-  description = "ECR repo name"
-  type        = string
-  default     = "wayfair-catalog"
+variable "vpc_cidr" {
+  default = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  default = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "instance_type" {
+  default = "t3.micro"
+}
+
+variable "key_name" {
+  default = "Suresh"
 }
